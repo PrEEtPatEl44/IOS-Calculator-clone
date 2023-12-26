d3568c4005a1328ef06f639862d6ce4e97f1f133
Preet Patel <pbpatel48@myseneca.ca>
2023-12-25 22:49:36 -0500

    Added JS

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
-
-let display = document.querySelector('#display');
+let display = document.querySelector("#result");
 function show(input) {
-   while(input!=='='){
-        if (input === 'C') {
-            display.value = '';
-        }else if (input==='+'||input==='-'||input==='*'||input==='/'){
-            display.value = '';
-            show(input);
-        }}
-        else{
-            
-        }
-    
+  display.value += input;
+}
+function clearD() {
+  display.value = "";
+}
+function calculate() {
+  try {
+    const result = evalExpression(display.value);
+
+    display.value = result;
+
+    return result;
+  } catch (error) {
+    display.value = "Error";
+    return "Error";
+  }
+}
+
+function evalExpression(expression) {
+  const tokens = expression.match(/(\d+(\.\d+)?|\+|\-|\*|\/|%)/g);
+
+  let result = parseFloat(tokens[0]);
+
+  for (let i = 1; i < tokens.length; i += 2) {
+    const operator = tokens[i];
+    const operand = parseFloat(tokens[i + 1]);
+
+    switch (operator) {
+      case "+":
+        result += operand;
+        break;
+      case "-":
+        result -= operand;
+        break;
+      case "*":
+        result *= operand;
+        break;
+      case "/":
+        result /= operand;
+        break;
+      case "%":
+        result %= operand;
+        break;
+    }
+  }
+
+  return result;
 }
